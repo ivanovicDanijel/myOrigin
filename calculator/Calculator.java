30a8d55c6f2b5115cef7281f23e6b7f8aabd53c6
Dzenan Helic <dzenan_helic@hotmail.com>
2017-06-01 11:01:07 +0200

    Ubacivanje metoda za korijen i kvadriranje

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -2,19 +2,37 @@ package calculator;
 
 public class Calculator {
 
-	public double add(double a,double b){
+	public double add(double a, double b) {
 		return a + b;
 	}
-	
-	public double substract(double a,double b){
+
+	public double substract(double a, double b) {
 		return a - b;
 	}
-	
-	public double multiply(double a,double b){
+
+	public double multiply(double a, double b) {
 		return a * b;
 	}
-	
-	public double divide(double a,double b){
+
+	public double divide(double a, double b) {
 		return a / b;
+	}
+
+	public double squareRoot(double number, double root) {
+
+		if (root == 0 || root == 1) {
+			return 1;
+		}
+
+		else {
+
+			return Math.pow(number, (1.0 / root));
+
+		}
+	}
+
+	public double POW(double number, double pow) {
+
+		return Math.pow(number, pow);
 	}
 }
